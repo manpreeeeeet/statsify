859ef60b3d36685bf08c67df13894e8070a12bbf
manpreet <manpreetrahal27@gmail.com>
2024-10-21 22:54:16 -0600

    init

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.0.21"
+    id("io.ktor.plugin") version "3.0.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
+}
+
+group = "manpreet"
+version = "0.0.1"
+
+application {
+    mainClass.set("manpreet.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-sessions-jvm")
+    implementation("io.ktor:ktor-server-host-common-jvm")
+    implementation("io.ktor:ktor-server-csrf-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("com.squareup.okhttp:okhttp:2.7.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-test-host-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
